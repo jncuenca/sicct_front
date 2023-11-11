1f9100ce94c4ee5b8341d6b0324cd6ecf07fb1bb
Juan Cuenca <juan99.cuenca@gmail.com>
2023-11-11 16:13:19 -0300

    Partial layout structure

--- src/sections/layout/Layout.jsx
+++ src/sections/layout/Layout.jsx
@@ -1,17 +1,26 @@
-import { Link } from 'react-router-dom'
 import './Layout.css'
+import { Link } from 'react-router-dom'
+import { BiUser, BiHomeAlt, BiMoney, BiTransfer, BiAddToQueue } from 'react-icons/bi'
 
 const Layout = ({ children }) => {
   return (
     <div className="app-container">
       <div className="sidebar">
-        <h2>Sidebar</h2>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/"><BiHomeAlt /><span>Inicio</span></Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/"><BiAddToQueue /><span>Registro interno</span></Link>
+          </li>
+          <li>
+            <Link to="/"><BiTransfer /><span>Cobros</span></Link>
+          </li>
+          <li>
+            <Link to="/"><BiMoney /><span>Control de Efectivo</span></Link>
+          </li>
+          <li>
+            <Link to="/mi-cuenta"><BiUser /><span>Mi cuenta</span></Link>
           </li>
         </ul>
       </div>
